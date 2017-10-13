fd2b684d237c562a31b576e14d7512d6c7a95108
VacheKhanoyan <31357910+VacheKhanoyan@users.noreply.github.com>
2017-10-13 16:07:39 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
  
 app.post('/like', function(req, res) {
     res.send('Like ++');
 });
   
-  app.listen(1234)
+  app.listen(1234);
